c457587881d0922a6682200aad37ab66d4c13ebb
Jason Wilkes <44591433+aScruffyMind@users.noreply.github.com>
2022-02-15 18:39:39 -0800

    Dynamic date in footer, and added Google Analytics code. (#4)

--- public/javascript/qr.js
+++ public/javascript/qr.js
@@ -1,10 +1,16 @@
+window.dataLayer = window.dataLayer || [];
+
+const today = new Date();
 const weblinks = {
     website: 'http://www.pentup.band',
     reverbnation: 'https://www.reverbnation.com/pentup',
     bandcamp: 'https://pentup-rock.bandcamp.com/'
 };
-
 const buttons = document.querySelectorAll('.button');
+
+function gtag() {
+    dataLayer.push(arguments);
+}
 
 // display Coming Soon if streaming buttons are clicked
 document.querySelectorAll('.listen-btn').forEach(x => {
@@ -12,3 +18,8 @@ document.querySelectorAll('.listen-btn').forEach(x => {
         document.querySelector('#coming-soon').classList.remove('hidden');
     });
 });
+
+document.querySelector('.copyright').innerHTML = `&copy;${today.getFullYear()}, Pent Up! All rights reserved.`;
+
+gtag('js', new Date());
+gtag('config', 'G-7QT7P357RK');
