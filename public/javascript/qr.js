455438a610135232e5cd8ae508b51b5fb0ee551b
Jason Wilkes <44591433+aScruffyMind@users.noreply.github.com>
2022-03-11 05:46:02 -0800

    Create functions to handle button clicks (DRY).

--- public/javascript/qr.js
+++ public/javascript/qr.js
@@ -12,51 +12,46 @@ function gtag() {
     dataLayer.push(arguments);
 }
 
-document.querySelectorAll('.button').forEach(x => {
-    x.addEventListener('click', () => {
-        switch (x.id) {
-            case 'applemusic':
-                window.open('https://music.apple.com/us/artist/pent-up/1613290339', '_blank');
-                break;
-            case 'spotify':
-                window.open('https://open.spotify.com/artist/4WPVheZNfKNT9plPbMUjQU?si=H8N5sgH9Rs-ppkL9vq9_Og', '_blank');
-                break;
-            case 'website':
-                window.open('http://www.pentup.band', '_self');
-                break;
-            case 'reverbnation':
-                window.open('https://www.reverbnation.com/pentup', '_blank');
-                break;
-            case 'bandcamp':
-                window.open('https://pentup-rock.bandcamp.com/', '_blank');
-                break;
-            default:
-                console.log(`Something went wrong.`);
-        }
-    });
-});
+function handleClick(id) {
+    const data = switches(id);
+    window.open(data.url, data.target);
+    console.log(data);
+}
 
-document.querySelectorAll('.social-icon').forEach(x => {
+function objectify(x, y) {
+    return {url: x, target: y};
+}
+
+function switches(id) {
+    switch (id) {
+        case 'applemusic':
+            return objectify('https://music.apple.com/us/artist/pent-up/1613290339', '_blank');
+        case 'spotify':
+            return objectify('https://open.spotify.com/artist/4WPVheZNfKNT9plPbMUjQU?si=H8N5sgH9Rs-ppkL9vq9_Og', '_blank');
+        case 'website':
+            return objectify('http://www.pentup.band', '_self');
+        case 'reverbnation':
+            return objectify('https://www.reverbnation.com/pentup', '_blank');
+        case 'bandcamp':
+            return objectify('https://pentup-rock.bandcamp.com/', '_blank');
+        case 'facebook':
+            return objectify('https://www.facebook.com/pentup.band/', '_blank');
+        case 'twitter':
+            return objectify('https://twitter.com/PentUpRock', '_blank');
+        case 'instagram':
+            return objectify('https://www.instagram.com/pent_up_band/', '_blank');
+        case 'youtube':
+            return objectify('https://www.youtube.com/channel/UCHd5r2W_8tvglPx4LJ4yKdA', '_blank');
+        case 'mail':
+            return objectify(`mailto:pent.up.rock@gmail.com?subject=You have an owl?!`, '_self');
+        default:
+            console.log(`Something went wrong.`);
+    }
+}
+
+document.querySelectorAll('.button, .social-icon').forEach(x => {
     x.addEventListener('click', () => {
-        switch (x.id) {
-            case 'facebook':
-                window.open('https://www.facebook.com/pentup.band/', '_blank');
-                break;
-            case 'twitter':
-                window.open('https://twitter.com/PentUpRock', '_blank');
-                break;
-            case 'instagram':
-                window.open('https://www.instagram.com/pent_up_band/', '_blank');
-                break;
-            case 'youtube':
-                window.open('https://www.youtube.com/channel/UCHd5r2W_8tvglPx4LJ4yKdA', '_blank');
-                break;
-            case 'mail':
-                window.open('mailto:pent.up.rock@gmail.com?subject=You have an owl?!', '_self');
-                break;
-            default:
-                console.log(`Something went wrong.`);
-        }
+        handleClick(x.id);
     });
 });
 
