15bcd3002c6f54e0027086b7e8713333b55e6587
Jason Wilkes <44591433+aScruffyMind@users.noreply.github.com>
2022-03-27 01:37:20 -0700

    Update to use JSON data to generate page.

--- public/javascript/qr.js
+++ public/javascript/qr.js
@@ -1,61 +1,81 @@
 window.dataLayer = window.dataLayer || [];
+const d = document;
+const store = [];
 
-const today = new Date();
-const buttons = document.querySelectorAll('.button');
-const sites = [
-    {name: 'Official Website', id: 'website', url: 'http://www.pentup.band', type: 'website', target: '_self'},
-    {name: 'ReverbNation', id: 'reverbnation', url: 'https://www.reverbnation.com/pentup', type: 'website', target: '_blank'},
-    {name: 'Bandcamp', id: 'bandcamp', url: 'https://pentup-rock.bandcamp.com/', type: 'website', target: '_blank'},
-    {name: 'Apple Music', id: 'applemusic', url: 'https://music.apple.com/us/artist/pent-up/1613290339', type: 'streaming', target: '_self'},
-    {name: 'Spotify', id: 'spotify', url: 'https://open.spotify.com/artist/4WPVheZNfKNT9plPbMUjQU?si=H8N5sgH9Rs-ppkL9vq9_Og', type: 'streaming', target: '_self'},
-    {name: 'Facebook', id: 'facebook', url: 'https://www.facebook.com/pentup.band/', type: 'social', target: '_blank'},
-    {name: 'Twitter', id: 'twitter', url: 'https://twitter.com/PentUpRock', type: 'social', target: '_blank'},
-    {name: 'Instagram', id: 'instagram', url: 'https://www.instagram.com/pent_up_band/', type: 'social', target: '_blank'},
-    {name: 'YouTube', id: 'youtube', url: 'https://www.youtube.com/channel/UCHd5r2W_8tvglPx4LJ4yKdA', type: 'social', target: '_blank'},
-    {name: 'Mail', id: 'mail', url: 'mailto:pent.up.rock@gmail.com?subject=You have an owl?!', type: 'social', target: '_self'}
-];
-
-function handleClick(id) {
-    const data = switches(id);
-    window.open(data.url, data.target);
-}
-
-function objectify(x, y) {
-    return {url: x, target: y};
-}
-
-function switches(id) {
-    switch (id) {
-        case 'applemusic':
-            return objectify('https://music.apple.com/us/artist/pent-up/1613290339', '_self');
-        case 'spotify':
-            return objectify('https://open.spotify.com/artist/4WPVheZNfKNT9plPbMUjQU?si=H8N5sgH9Rs-ppkL9vq9_Og', '_self');
-        case 'website':
-            return objectify('http://www.pentup.band', '_self');
-        case 'reverbnation':
-            return objectify('https://www.reverbnation.com/pentup', '_blank');
-        case 'bandcamp':
-            return objectify('https://pentup-rock.bandcamp.com/', '_blank');
-        case 'facebook':
-            return objectify('https://www.facebook.com/pentup.band/', '_blank');
-        case 'twitter':
-            return objectify('https://twitter.com/PentUpRock', '_blank');
-        case 'instagram':
-            return objectify('https://www.instagram.com/pent_up_band/', '_blank');
-        case 'youtube':
-            return objectify('https://www.youtube.com/channel/UCHd5r2W_8tvglPx4LJ4yKdA', '_blank');
-        case 'mail':
-            return objectify(`mailto:pent.up.rock@gmail.com?subject=You have an owl?!`, '_self');
-        default:
-            console.log(`Something went wrong.`);
-    }
-}
-
-document.querySelectorAll('.button, .social-icon').forEach(x => {
-    x.addEventListener('click', () => {
-        handleClick(x.id);
+function addListeners() {
+    d.querySelectorAll('.button, .social-icon').forEach(x => {
+        x.addEventListener('click', () => {
+            handleClick(x.id);
+        });
     });
-});
+}
 
-document.querySelector('.copyright').innerHTML = `&copy;${today.getFullYear()}, Pent Up! All rights reserved.`;
-document.querySelector('.newsflash').innerHTML = `New single 'Self-Titled' out now on all major streaming services!`;
+// This function generates all the buttons on the page and then adds listeners to them.
+function generateButtons(buttonData) {
+    const dFrag = d.createDocumentFragment();  
+    buttonData.forEach((v, i, a) => {
+        const button = d.createElement('div');
+        const buttonLabel = d.createElement('span');
+        button.classList.add('button');
+        button.setAttribute('id', v.id);
+        buttonLabel.innerHTML = v.name;
+        button.appendChild(buttonLabel);
+        dFrag.appendChild(button);
+    });
+    return dFrag;
+}
+
+// This function generates the Streaming Service buttons at the top of the page
+function generatePrimaryStreamers(streamerData) {
+    const primaries = streamerData.filter(x => x.id === 'applemusic' || x.id === 'spotify');
+    const dFrag = d.createDocumentFragment();
+    primaries.forEach((v, i, a) => {
+        const streamer = d.createElement('img');
+        streamer.setAttribute('id', v.id);
+        streamer.classList.add('listen-btn', 'button');
+        streamer.src = `/images/buttons/${v.id}_btn.png`;
+        streamer.setAttribute('alt', v.alt);
+        dFrag.appendChild(streamer);
+    });
+    return dFrag;
+}
+
+// This function generates the social buttons along the bottom of the page.
+function generateSocials(socialData) {
+    const dFrag = d.createDocumentFragment();
+    socialData.forEach((v, i, a) => {
+        const image = d.createElement('img');
+        image.setAttribute('id', v.id);
+        image.classList.add('social-icon');
+        image.src = `/images/icons/icon-${v.id}.png`;
+        v.alt === undefined ? image.setAttribute('alt', v.name) : image.setAttribute('alt', v.alt);
+        dFrag.appendChild(image);
+    });
+    return dFrag;
+}
+
+function getData() {
+    fetch('/javascript/qr_data.json')
+        .then(response => response.json())
+        .then(jsondata => populateData(jsondata));
+}
+
+function handleClick(id) {   
+    const site = store.find(x => x.id === id);
+    window.open(site.url, site.target);
+}
+
+function populateData(data) {
+    data.sites.forEach(x => {
+        store.push({id: x.id, url: x.url, target: x.target});        
+    });
+
+    d.querySelector('.newsflash').innerHTML = data.news;
+    d.getElementById('buttons').appendChild(generateButtons(data.sites.filter(x => x.type === 'website')));
+    d.getElementById('socials').appendChild(generateSocials(data.sites.filter(x => x.type === 'social')));
+    d.getElementById('streaming').prepend(generatePrimaryStreamers(data.sites.filter(x => x.type === 'streaming')));
+    addListeners();
+}
+
+getData();
+d.querySelector('.copyright').innerHTML = `&copy;${new Date().getFullYear()}, Pent Up! All rights reserved.`;
